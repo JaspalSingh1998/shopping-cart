663e7bd8089eca9a9928eb9fb4d01878d26dcdcd
Jaspal Singh <jaspalsinghpersonal9@gmail.com>
2021-08-14 14:58:52 +0530

    Finished Remove From Cart Functionality

--- shoppingCart.js
+++ shoppingCart.js
@@ -1,21 +1,30 @@
 import items from "./items.json";
 import formatCurrency from "./util/formatCurrency";
+import addGlobalEventListener from "./util/addGlobalEventListener";
+
 const cartButton = document.querySelector("[data-cart-button]");
 const cartItemsWrapper = document.querySelector("[data-cart-items-wrapper]");
 const cartItemTemplate = document.querySelector("#cart-item-template");
 const cartItemContainer = document.querySelector("[data-cart-items-container]");
 const cartQuantity = document.querySelector("[data-cart-quantity]");
 const cartTotal = document.querySelector("[data-cart-total]");
+const cart = document.querySelector("[data-cart]");
+
 const IMAGE_URL = "https://dummyimage.com/210x130";
 let shoppingCart = [];
 
-export function setupShoppingCart() {}
+export function setupShoppingCart() {
+  renderCart();
+}
+
+addGlobalEventListener("click", "[data-remove-from-cart-button]", (e) => {
+  const id = e.target.closest("[data-item]").dataset.itemId;
+  removeFromCart(parseInt(id));
+});
 
 // Remove items from cart
 // Show/Hide the cart when it has no items or when it goes from 0 to 1 item
 // Persist across multiple pages
-// Calculate an accurate total
-// Handle multiple of the same item in the cart
 
 // show/Hide cart when clicked
 cartButton.addEventListener("click", () => {
@@ -33,7 +42,31 @@ export function addToCart(id) {
   renderCart();
 }
 
+function removeFromCart(id) {
+  const existingItem = shoppingCart.find((entry) => entry.id === id);
+  if (existingItem == null) return;
+  shoppingCart = shoppingCart.filter((entry) => entry.id !== id);
+  renderCart();
+}
+
 function renderCart() {
+  if (shoppingCart.length === 0) {
+    hideCart();
+  } else {
+    showCart();
+    renderCartItems();
+  }
+}
+
+function hideCart() {
+  cart.classList.add("invisible");
+  cartItemsWrapper.classList.add("invisible");
+}
+function showCart() {
+  cart.classList.remove("invisible");
+}
+
+function renderCartItems() {
   cartQuantity.textContent = shoppingCart.length;
   const totalCents = shoppingCart.reduce((sum, entry) => {
     const item = items.find((i) => entry.id === i.id);
